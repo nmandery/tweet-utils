ff09aeea787d099aea4577ed2d24e3fffee3502c
Nico Mandery <nico@nmandery.net>
2022-05-03 19:48:16 +0200

    remove currently unused lightgbm training

--- twitter-user-movement/src/main.rs
+++ twitter-user-movement/src/main.rs
@@ -10,7 +10,6 @@ use clap::{Args, Parser, Subcommand};
 use eyre::Report;
 use geo_types::{Coordinate, LineString};
 use geojson::{Feature, FeatureCollection, GeoJson, Value};
-use lightgbm::{Booster, Dataset};
 use serde::Deserialize;
 use serde_json::{json, to_value, Map};
 use std::collections::hash_map::{Entry, RandomState};
@@ -36,31 +35,12 @@ enum Command {
     ///
     /// The JSON will be written to stdout
     ToGeoJson(FileList),
-
-    Train(TrainArgs),
 }
 
 #[derive(Args, Debug)]
 struct FileList {
     /// JSONL files containing tweets
     jsonl_files: Vec<String>,
-}
-
-#[derive(Args, Debug)]
-struct TrainArgs {
-    training_config_file: String,
-
-    /// JSONL files containing tweets
-    jsonl_files: Vec<String>,
-}
-
-#[derive(Deserialize, Debug)]
-struct TrainingConfig {
-    /// screen names of users to use for the positive selection
-    positive_user_screen_names: Vec<String>,
-
-    /// screen names of users to use for the positive selection
-    negative_user_screen_names: Vec<String>,
 }
 
 type Movements = HashMap<u64, UserMovement>;
@@ -76,13 +56,6 @@ fn main() -> eyre::Result<()> {
         Command::ToMovementJson(file_list) => {
             let movements = parse_movements(&file_list.jsonl_files)?;
             println!("{}", serde_json::to_string(&movements)?);
-        }
-        Command::Train(train_args) => {
-            let movements = parse_movements(&train_args.jsonl_files)?;
-            let training_config =
-                serde_json::from_reader(File::open(&train_args.training_config_file)?)?;
-            let booster = train(&movements, &training_config)?;
-            todo!("apply booster to data")
         }
     }
     Ok(())
@@ -199,47 +172,4 @@ fn save_geojson(user_movements: HashMap<u64, UserMovement>) -> eyre::Result<()> 
 
     println!("{}", gj);
     Ok(())
-}
-
-fn train(movements: &Movements, training_config: &TrainingConfig) -> eyre::Result<Booster> {
-    let positive_screen_name_set: HashSet<String, RandomState> =
-        HashSet::from_iter(training_config.positive_user_screen_names.iter().cloned());
-    let negative_screen_name_set: HashSet<String, RandomState> =
-        HashSet::from_iter(training_config.negative_user_screen_names.iter().cloned());
-
-    let mut n_positive = 0u64;
-    let mut n_negative = 0u64;
-    let mut training_data = vec![];
-    let mut labels = vec![];
-
-    for user_movement in movements.values() {
-        if positive_screen_name_set.contains(&user_movement.user_screen_name) {
-            training_data.push(user_movement.metrics().to_vec());
-            labels.push(1.0f32);
-            n_positive += 1;
-        } else if negative_screen_name_set.contains(&user_movement.user_screen_name) {
-            training_data.push(user_movement.metrics().to_vec());
-            labels.push(0.0f32);
-            n_negative += 1;
-        }
-    }
-    eprintln!(
-        "Found {} positive and {} negative labeled user movements",
-        n_positive, n_negative
-    );
-    if training_data.is_empty() {
-        return Err(Report::msg("found no training data"));
-    }
-    dbg!(&training_data);
-    let dataset = Dataset::from_mat(training_data, labels)?;
-    let params = json! {
-        {
-            "num_iterations": 100usize,
-            "objective": "binary",
-            "metric": "auc"
-        }
-    };
-    let booster = Booster::train(dataset, &params)?;
-
-    Ok(booster)
 }
